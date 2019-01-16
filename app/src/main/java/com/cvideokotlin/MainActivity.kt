f7a6eb83554c1880f09024e3656e613518452111
ksonCode <19655910@qq.com>
2019-01-17 00:17:11 +0800

    login

--- app/src/main/java/com/cvideokotlin/MainActivity.kt
+++ app/src/main/java/com/cvideokotlin/MainActivity.kt
@@ -2,12 +2,16 @@ package com.cvideokotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
 
--- app/src/main/java/com/cvideokotlin/Test.kt
+++ app/src/main/java/com/cvideokotlin/Test.kt
@@ -2,9 +2,27 @@ package com.cvideokotlin
 
 data class Test(val id:Int,val name: String)
 
-fun main(args:Array<String>){
+fun main(args:Array<String>):Unit{
     println("hello ")
 
-    println(Test(1,"hello"))
+    var test = Test(1,"hello")
 
+    println(test.name)
+
+    var i :Int =2
+    println(i)
+
+    var ii = 2;
+    println(ii)
+
+    println(hello(5))
+
+    var intArray = IntArray(5)
+    var anotherArray = intArrayOf(1,2,3,4,5)
+
+
+}
+
+fun hello(id: Int):Unit{
+    println(id)
 }
